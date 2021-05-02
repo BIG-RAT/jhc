2734e9da49e9903a7014f77ac51a321ad9beaa9d
Leslie Helou <leslie@ccs-indy.com>
2021-05-02 18:44:18 -0400

    Initial Commit

---
+++ jhc/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  jhc
+//
+//  Created by Leslie Helou on 5/2/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

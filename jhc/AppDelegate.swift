a5e131d98474aa22384904842866ac5b51ab62e0
Leslie Helou <leslie@ccs-indy.com>
2026-01-15 21:54:47 -0500

    Update AppDelegate.swift

--- jhc/AppDelegate.swift
+++ jhc/AppDelegate.swift
@@ -31,8 +31,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         }
     }
     
-
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
         configureTelemetryDeck()
